81364abdb7ae61ae2c5cac91cb901a285d5d4ffc
ChanningTong <Channing_TongCN@outlook.com>
2024-12-10 13:09:46 +0800

    Initial Commit

---
+++ RSimageprocess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RSimageprocess
+//
+//  Created by ChanningTong on 12/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
